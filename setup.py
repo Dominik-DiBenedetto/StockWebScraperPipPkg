3571f1c32460dcada0469358f4fc11b9240edfc0
YT-The-Scripting-Legend <69871335+YT-The-Scripting-Legend@users.noreply.github.com>
2021-02-12 18:14:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'stock-webscraper',         # How you named your package folder (MyLib)
   packages = ['stock-webscraper'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Use this to scrape stock prices',   # Give a short description about your library
   author = 'The Scripting Legend',                   # Type in your name

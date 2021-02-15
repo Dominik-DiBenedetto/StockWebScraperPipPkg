bef931d523645c7143df3948e2bbcd003c800331
YT-The-Scripting-Legend <69871335+YT-The-Scripting-Legend@users.noreply.github.com>
2021-02-15 09:12:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'stockwebscraper',         # How you named your package folder (MyLib)
   packages = ['stockwebscraper'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.2.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Use this to scrape stock prices',   # Give a short description about your library
   author = 'The Scripting Legend',                   # Type in your name
   author_email = 'thescriptinglegend@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/YT-The-Scripting-Legend/StockWebScraperPipPkg',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/YT-The-Scripting-Legend/StockWebScraperPipPkg/archive/v_02.tar.gz',    # The realease assets URL for download
+  download_url = 'https://github.com/YT-The-Scripting-Legend/StockWebScraperPipPkg/archive/v_0.2.1.tar.gz',    # The realease assets URL for download
   keywords = ['STOCKS', 'STOCK', 'STOCK MARKET', 'WEB', 'SCRAPING', 'WEB SCRAPING'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'beautifulsoup4',
